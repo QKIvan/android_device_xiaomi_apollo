d8077982b3d1aaa98df81aa353338570d164f63e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-11 22:07:39 +0100

    apollo: media: Import QTI Codec2 configs
    Change-Id: I17a56c596f15a1d5c23582c5c3a7028d3e676aab

--- device.mk
+++ device.mk
@@ -217,6 +217,10 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.odm.build.media_performance_class=30 \
     vendor.media.omx=0
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
+
 # Keymaster
 PRODUCT_VENDOR_PROPERTIES += \
     ro.crypto.allow_encrypt_override=true \
